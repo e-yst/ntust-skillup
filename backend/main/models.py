52ca22417ec80f13b3f15feeb894cdd1aade1277
Eason Tse <shuntys92@gmail.com>
2017-05-31 01:49:17 +0800

    restructured

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,13 +3,43 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    pic_loc = models.CharField(max_length=150)
+
+
+class Teacher(models.Model):
+    member = models.ForeignKey(User)
+    good_at = models.ManyToManyField(Category)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    posted_on = models.DateTimeField()
+    start_from = models.DateField()
+    end_on = models.DateField()
+    deadline = models.DateTimeField()
+    hosted_by = models.ManyToManyField(Teacher)
+    categories = models.ManyToManyField(Category)
+    click_counter = models.IntegerField(default=0)
+
+
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.BooleanField()
     dob = models.DateField()
     user_bio = models.TextField()
+    interested_in = models.ManyToManyField(Category)
+    lessons_joint = models.ManyToManyField(Lesson)
 
 
-class Interest(models.Model):
-    name = models.CharField(max_length=50)
-    user = models.ManyToManyField(Member)
+class Wish(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category)
+    posted_by = models.ForeignKey(User)
+    posted_on = models.DateTimeField()
+    click_counter = models.IntegerField(default=0)
